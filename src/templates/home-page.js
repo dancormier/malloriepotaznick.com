0f9795afdc2f374617f17086f4fd92b8822b5aa8
Dan Cormier <dancormierall@gmail.com>
2019-01-28 10:53:15 -0500

    Add homepage template

---
+++ src/templates/home-page.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Features from '../components/Features'
+
+export const ProductPageTemplate = ({
+  heading,
+  main,
+}) => (
+  <section className="section section--gradient">
+    <div className="container">
+      <div className="section">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="content">
+              <div className="columns">
+                <div className="column is-7">
+                  <h3 className="has-text-weight-semibold is-size-2">
+                    {heading}
+                  </h3>
+                </div>
+              </div>
+              <Features gridItems={main.blurbs} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+ProductPageTemplate.propTypes = {
+  heading: PropTypes.string,
+  main: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
+}
+
+const ProductPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <ProductPageTemplate
+        heading={frontmatter.heading}
+        main={frontmatter.main}
+      />
+    </Layout>
+  )
+}
+
+ProductPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default ProductPage
+
+export const productPageQuery = graphql`
+  query ProductPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        heading
+        main {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+        }
+      }
+    }
+  }
+`
