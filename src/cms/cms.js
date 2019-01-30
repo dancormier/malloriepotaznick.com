5f679fb7e63cad192176729640c0c31b2d2c11f7
Dan Cormier <dancormierall@gmail.com>
2019-01-30 15:19:18 -0500

    add background color, image to hero

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,10 @@
 import CMS from 'netlify-cms'
+import * as ColorWidget from "netlify-cms-widget-color";
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+
+CMS.registerWidget("color", ColorWidget.Control);
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
