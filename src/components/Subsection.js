2c0cd1f8a2db5414f6253a65b7c53ca52f5b1d7a
Dan Cormier <dancormierall@gmail.com>
2019-02-04 18:17:57 -0500

    Add Subsection component

---
+++ src/components/Subsection.js
@@ -0,0 +1,42 @@
+// import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import theme from './theme'
+import Heading from './Heading'
+import Markdown from './Markdown'
+
+const Subsection = ({
+  customCSS,
+  children,
+  heading,
+}) => (
+  <div
+    css={{
+      ...customCSS,
+    }}
+  >
+    <Heading
+      Tag='h4'
+      css={{
+        color: theme.color('accent'),
+        fontSize: theme.size(4),
+        marginBottom: theme.size(4),
+      }}
+    >
+      {heading}
+    </Heading>
+    <div
+      css={{
+        fontSize: theme.size(2),
+        lineHeight: theme.size(4),
+        marginBottom: theme.size(8),
+      }}
+    >
+      <Markdown>
+        {children}
+      </Markdown>
+    </div>
+  </div>
+);
+
+export default Subsection;
