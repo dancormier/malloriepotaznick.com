f4f131b960f97f5fc8cfd4172c26aaa2f7d584a4
Dan Cormier <dancormierall@gmail.com>
2019-02-06 14:34:43 -0500

    Clean up head tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,41 +20,44 @@ const TemplateWrapper = ({ children }) => (
           }
         }
     `}
-    render={data => (
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <title>{data.site.siteMetadata.title}</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
+    render={data => {
+      const {
+        description,
+        title,
+      } = data.site.siteMetadata;
 
-	        <meta property="og:type" content="business.business" />
-          <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.jpg" />
-          <meta
-            name="google-site-verification"
-            content="qvbZgNAq97PJ5oLAOwHLVDZA1PXG8tPjybFIhZ_37WU"
-          />
-        </Helmet>
-        <ThemeProvider theme={theme}>
-          <div
-            css={{
-              fontFamily: theme.font('serif'),
-            }}
-          >
-            <Navbar />
-            <div>{children}</div>
-          </div>
-        </ThemeProvider>
-      </div>
-    )}
+      return (
+        <div>
+          <Helmet>
+            <html lang="en" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            
+            <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+            <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+    
+            <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+            <meta name="theme-color" content="#fff" />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={title} />
+            <meta property="og:url" content="/" />
+            <meta property="og:image" content="/img/og-image.jpg" />
+            <meta
+              name="google-site-verification"
+              content="qvbZgNAq97PJ5oLAOwHLVDZA1PXG8tPjybFIhZ_37WU"
+            />
+          </Helmet>
+          <ThemeProvider theme={theme}>
+            <div css={{ fontFamily: theme.font('serif') }}>
+              <Navbar />
+              <div>{children}</div>
+            </div>
+          </ThemeProvider>
+        </div>
+      );
+    }}
   />
 )
 
