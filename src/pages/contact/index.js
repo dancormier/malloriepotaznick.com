76b8cfd91ef7a4179f90013430dc0cec34c779f2
Dan Cormier <dancormierall@gmail.com>
2019-02-13 11:50:55 -0500

    remove unused encode func

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -3,12 +3,6 @@ import Layout from '../../components/Layout'
 import Container from '../../components/Container'
 import ContactForm from '../../components/Contact-form'
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
-
 export default class Index extends React.Component {
   render() {
     return (
