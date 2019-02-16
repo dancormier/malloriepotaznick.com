e0eb2dccb8471ae640f89337fb005bc94e1a8e71
Dan Cormier <dancormierall@gmail.com>
2019-02-16 11:29:50 -0500

    get rid of subsection heading

--- src/templates/_home-page.js
+++ src/templates/_home-page.js
@@ -97,7 +97,6 @@ const HomePage = () => (
                   }
                   subsections {
                     body
-                  	heading
                   }
                 }
               }
