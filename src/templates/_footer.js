4aa237287656a13f29fdcd25ee8a951b11549021
Dan Cormier <dancormierall@gmail.com>
2019-02-25 14:49:03 -0500

    update footer template

--- src/templates/_footer.js
+++ src/templates/_footer.js
@@ -5,27 +5,26 @@ import PropTypes from 'prop-types';
 import { ThemeProvider } from 'emotion-theming';
 import Container from '../components/Container';
 import FooterContact from '../components/FooterContact';
+import Markdown from '../components/Utility/Markdown';
 import theme from '../components/Utility/theme';
 
 const group = (items, grouping) => items.filter(item => item.align === grouping);
 const FooterItem = ({ item }) => {
   const {
     align,
-    text,
+    body,
   } = item;
 
   return (
     <ThemeProvider theme={theme}>
-      <div
-        key={text}
-        css={{
-          alignItems: 'center',
-          display: 'flex',
-          marginRight: align === 'left' && theme.size(2),
-          marginLeft: align === 'right' && theme.size(2),
-        }}
-      >
-        {text}
+      <div css={{ textAlign: align }}>
+        <Markdown
+          customCSS={{
+            fontFamily: theme.font('sans'),
+          }}
+        >
+          {body}
+        </Markdown>
       </div>
     </ThemeProvider>
   );
@@ -34,53 +33,79 @@ const FooterItem = ({ item }) => {
 export const FooterTemplate = ({
   body,
   buttons,
+  copyright,
   items,
   showFooterContact,
-}) => (
-  <ThemeProvider theme={theme}>
-    <div>
-      {showFooterContact && (
-        <FooterContact
-          body={body}
-          buttons={buttons}
-          text="Testy!"
-        />
-      )}
-      <div
-        css={{
-          backgroundColor: theme.color('accent'),
-          color: theme.color('white'),
-        }}
-      >
-        <Container
-          customCSS={{
-            display: 'flex',
-            fontFamily: theme.font('sans'),
-            fontSize: theme.size(0),
-            justifyContent: 'space-between',
-            paddingTop: theme.size(1),
-            paddingBottom: theme.size(1),
-          }}
-        >
-          <div>
-            {group(items, 'left').map(item =>
-              <FooterItem item={item} key={item.text} />
-            )}
-          </div>
-          <div css={{ display: 'flex' }}>
-            {group(items, 'right').map(item =>
-              <FooterItem item={item} key={item.text} />
-            )}
-          </div>
-        </Container>
+}) => {
+  const year = new Date().getFullYear();
+  const legal = {
+    body: `© ${year} ${copyright}`,
+    align: 'left',
+  };
+  const itemsLeft = items && group(items, 'left');
+  const copyItem = group([legal], 'left');
+  const itemsRight = items && group(items, 'right');
+  const itemsLeftCombined = itemsLeft ? [...itemsLeft, ...copyItem] : copyItem;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        {showFooterContact && (
+          <FooterContact
+            body={body}
+            buttons={buttons}
+          />
+        )}
+        <div>
+          <Container
+            customCSS={{
+              fontFamily: theme.font('sans'),
+              marginTop: theme.size(2),
+              paddingBottom: theme.size(4),
+              paddingTop: theme.size(2),
+            }}
+          >
+            <div
+              css={{
+                alignItems: 'flex-end',
+                display: 'flex',
+                justifyContent: 'space-between',
+                paddingBottom: theme.size(1),
+              }}
+            >
+              <div
+                css={{
+                  width: '33%',
+                }}
+              >
+                {itemsLeftCombined.map(item =>
+                  <FooterItem item={item} key={item.body} />
+                )}
+              </div>
+              {itemsRight && (
+                <div
+                css={{
+                  textAlign: 'center',
+                  width: '33%',
+                }}
+              >
+                  {group(items, 'right').map(item =>
+                    <FooterItem item={item} key={item.body} />
+                  )}
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
       </div>
-    </div>
-  </ThemeProvider>
-);
+    </ThemeProvider>
+  );
+};
 
 FooterTemplate.propTypes = {
   body: PropTypes.string,
   buttons: PropTypes.any,
+  copyright: PropTypes.string,
   items: PropTypes.any,
   showContactFooter: PropTypes.bool,
 }
@@ -101,9 +126,10 @@ const Footer = ({ showFooterContact }) => (
                   type
                   url
                 }
+                copyright
                 items {
                   align
-                  text
+                  body
                 }
               }
               rawMarkdownBody
@@ -117,6 +143,7 @@ const Footer = ({ showFooterContact }) => (
       const { rawMarkdownBody: body, frontmatter } = edges[0].node;
       const {
         buttons,
+        copyright,
         items,
       } = frontmatter;
 
@@ -124,6 +151,7 @@ const Footer = ({ showFooterContact }) => (
         <FooterTemplate
           body={body}
           buttons={buttons}
+          copyright={copyright}
           items={items}
           showFooterContact={showFooterContact}
         />
