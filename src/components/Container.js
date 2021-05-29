e5c7cb4eaf4b858b28b66be33f669e598330e160
Dan Cormier <dancormierall@gmail.com>
2021-05-29 12:33:52 -0400

    update node-sass; add .eslint config

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,30 +1,25 @@
 // import React from 'react'
 /** @jsx jsx */
-import { jsx } from '@emotion/react'
-import theme from './Utility/theme'
+import { jsx } from "@emotion/react";
+import theme from "./Utility/theme";
 
 const style = {
-  maxWidth: theme.prop('max'),
-  margin: 'auto',
+  maxWidth: theme.prop("max"),
+  margin: "auto",
   paddingLeft: theme.size(3),
   paddingRight: theme.size(3),
-  [theme.mq('sm')]: {
+  [theme.mq("sm")]: {
     paddingLeft: theme.size(6),
     paddingRight: theme.size(6),
   },
-  [theme.mq('xl')]: {
+  [theme.mq("xl")]: {
     paddingLeft: 0,
     paddingRight: 0,
   },
 };
 
-const Container = ({
-  customCSS,
-  children,
-}) => (
-  <div css={{...style, ...customCSS}}>
-    {children}
-  </div>
+const Container = ({ customCSS, children }) => (
+  <div css={{ ...style, ...customCSS }}>{children}</div>
 );
 
 export default Container;
